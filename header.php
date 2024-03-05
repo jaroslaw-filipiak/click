7990dcc46f8178ca3d1572b861fb25c18cc0938e
jaroslaw-filipiak <info@j-filipiak.pl>
2024-03-05 10:07:39 +0100

    add acf-json folder + check syncing

--- header.php
+++ header.php
@@ -1,23 +1,24 @@
 <?php
+
 /**
  * @package click5
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; 
+if (!defined('ABSPATH')) {
+	exit;
 }
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?>">
+    <meta name="apple-mobile-web-app-title" content="<?php bloginfo('name'); ?>">
     <link rel="profile" href="http://gmpg.org/xfn/11">
-    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
     <meta name="designer" content="click5 Interactive LLC - www.click5interactive.com" />
 
@@ -28,7 +29,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     <?php wp_body_open(); ?>
 
     <div class="site" id="page">
-        <?php if( get_field("hello_bar_enable", "options") == 1 ) : ?>
+        <?php if (get_field("hello_bar_enable", "options") == 1) : ?>
 
         <div id="hello-bar">
             <a href="<?php echo get_field("hello_bar_url", "options") ?>">
@@ -39,38 +40,38 @@ if ( ! defined( 'ABSPATH' ) ) {
         <?php endif; ?>
 
         <header
-            class="sticky-top d-flex justify-content-between align-items-center<?php echo ( get_field("hello_bar_enable", "options") == 1 ) ? ' hello-bar' : ''; ?>"
+            class="sticky-top d-flex justify-content-between align-items-center<?php echo (get_field("hello_bar_enable", "options") == 1) ? ' hello-bar' : ''; ?>"
             id="header" itemscope itemtype="http://schema.org/WebSite">
             <a class="skip-link sr-only sr-only-focusable"
-                href="#content"><?php esc_html_e( 'Skip to content', 'click5' ); ?></a>
+                href="#content"><?php esc_html_e('Skip to content', 'click5'); ?></a>
 
             <div class="logo">
-                <a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>"
-                    title="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" itemprop="url">Logo</a>
+                <a rel="home" href="<?php echo esc_url(home_url('/')); ?>"
+                    title="<?php echo esc_attr(get_bloginfo('name')); ?>" itemprop="url">Logo</a>
             </div>
 
             <div class="navigation d-flex justify-content-end align-items-center">
                 <nav class="navbar navbar-expand-xl">
                     <button class="navbar-toggler collapsed" type="button" data-toggle="collapse"
                         data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
-                        aria-label="<?php esc_attr_e( 'Toggle navigation', 'click5' ); ?>">
+                        aria-label="<?php esc_attr_e('Toggle navigation', 'click5'); ?>">
                         <span class="icon-bar top-bar"></span>
                         <span class="icon-bar middle-bar"></span>
                         <span class="icon-bar bottom-bar"></span>
                     </button>
 
                     <?php wp_nav_menu(
-					array(
-						'theme_location'  => 'primary',
-						'container_class' => 'collapse navbar-collapse',
-						'container_id'    => 'navbarNavDropdown',
-						'menu_class'      => 'navbar-nav ml-auto',
-						'fallback_cb'     => '',
-						'menu_id'         => 'main-menu',
-						'depth'           => 3,
-						'walker'          => new click5_WP_Bootstrap_Navwalker(),
-					)
-				); ?>
+						array(
+							'theme_location'  => 'primary',
+							'container_class' => 'collapse navbar-collapse',
+							'container_id'    => 'navbarNavDropdown',
+							'menu_class'      => 'navbar-nav ml-auto',
+							'fallback_cb'     => '',
+							'menu_id'         => 'main-menu',
+							'depth'           => 3,
+							'walker'          => new click5_WP_Bootstrap_Navwalker(),
+						)
+					); ?>
                 </nav>
 
                 <div class="phone">
@@ -80,8 +81,17 @@ if ( ! defined( 'ABSPATH' ) ) {
                     </a>
                 </div>
 
-                <div class="contact">
-                    <a href="<?php the_permalink(20); ?>">Contact Us Today!</a>
+                <div class="quote">
+                    <a href="">
+                        Get A Free Quote
+                    </a>
                 </div>
+
+                <div class="schedule">
+                    <a href="">
+                        Schedule Service
+                    </a>
+                </div>
+
             </div>
         </header>
