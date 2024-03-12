e2d1b567c66c20b5bc353b4f2c653af46f4616a7
jaroslaw-filipiak <info@j-filipiak.pl>
2024-03-12 11:10:17 +0100

    cta home rwd

--- page-templates/home.php
+++ page-templates/home.php
@@ -25,8 +25,7 @@ endif;
 
 <!-- hero -->
 <section>
-    <div class="wrapper d-flex align-items-end align-items-lg-center align-items-xl-end hero" id="hero"
-        style="background-image: url('<?php echo click5_check_background(); ?>')">
+    <div class="wrapper d-flex align-items-end align-items-lg-center align-items-xl-end hero" id="hero" style="background-image: url('<?php echo click5_check_background(); ?>')">
         <div class="container">
             <div class="row">
                 <div class="col-12">
@@ -37,8 +36,7 @@ endif;
             </div>
             <div class="row hero__buttons d-flex align-items-center">
                 <div class="col d-flex flex-column flex-sm-row align-items-center">
-                    <a target="<?php echo esc_attr($link_target); ?>" class="hero__buttons--schedule"
-                        href="<?php echo esc_url($link_url); ?>"><?php echo esc_html($link_title); ?></a>
+                    <a target="<?php echo esc_attr($link_target); ?>" class="hero__buttons--schedule" href="<?php echo esc_url($link_url); ?>"><?php echo esc_html($link_title); ?></a>
                     <a class="hero__buttons--call" href="tel:<?php echo get_field("c5ts_phone", "option"); ?>">Call
                         <span><?php echo get_field("c5ts_phone", "option"); ?></span></a>
                 </div>
@@ -72,22 +70,21 @@ endif;
                         $url = get_sub_field('url');
                 ?>
 
-                <div class="services-item">
-                    <div class="services-item__header" style="background-image: url('<?php echo $bg ?>')">
-                        <div class="services-item__icon">
-                            <img class="img-fluid" loading="lazy" src="<?php echo $icon; ?>" role="presentation">
+                        <div class="services-item">
+                            <div class="services-item__header" style="background-image: url('<?php echo $bg ?>')">
+                                <div class="services-item__icon">
+                                    <img class="img-fluid" loading="lazy" src="<?php echo $icon; ?>" role="presentation">
+                                </div>
+                            </div>
+                            <div class="<?php echo $label ? 'services-item__body services-item__body--has-label' : 'services-item__body' ?>">
+                                <?php if ($label) : ?>
+                                    <p class=" services-item__label"><?php echo $label ?></h4>
+                                    <?php endif; ?>
+                                    <h4><?php echo $title ?></h4>
+                                    <p><?php echo $excerpt ?></p>
+                                    <a href="<?php echo $url ?>">Learn More</a>
+                            </div>
                         </div>
-                    </div>
-                    <div
-                        class="<?php echo $label ? 'services-item__body services-item__body--has-label' : 'services-item__body' ?>">
-                        <?php if ($label) : ?>
-                        <p class=" services-item__label"><?php echo $label ?></h4>
-                            <?php endif; ?>
-                        <h4><?php echo $title ?></h4>
-                        <p><?php echo $excerpt ?></p>
-                        <a href="<?php echo $url ?>">Learn More</a>
-                    </div>
-                </div>
 
                 <?php endwhile;
 
@@ -101,11 +98,10 @@ endif;
                 <div class="col-12 text-center">
                     <?php echo get_field("cta_heading", "option"); ?>
                 </div>
-                <div class="col d-flex align-items-center justify-content-center mt-2">
+                <div class="col d-flex flex-column-reverse flex-md-row align-items-center justify-content-center mt-2">
                     <?php $link = get_field('schedule_service', 'option'); ?>
 
-                    <a class="cta--schedule-btn" target="<?php echo esc_attr($link_target); ?>"
-                        href="<?php echo esc_url($link_url); ?>"><?php echo esc_html($link_title); ?></a>
+                    <a class="cta--schedule-btn" target="<?php echo esc_attr($link_target); ?>" href="<?php echo esc_url($link_url); ?>"><?php echo esc_html($link_title); ?></a>
                     <a class="cta--call-btn" href="tel:<?php echo get_field("c5ts_phone", "option"); ?>">Call&nbsp;
                         <span><?php echo get_field("c5ts_phone", "option"); ?></span></a>
                 </div>
@@ -127,7 +123,7 @@ if (have_rows('textboxes')) :
         $text = get_sub_field('text');
         $button = get_sub_field('button');
 
-        $offset = get_row_index() % 2 == 0 ? 'offset-0 col-lg-6' : 'offset-2 col-lg-4';
+        $offset = get_row_index() % 2 == 0 ? 'offset-0 col-lg-6' : 'offset-lg-2 col-lg-4';
         $align = get_row_index() % 2 == 0 ? 'd-flex justify-content-end pr-5' : NULL;
         $pt = get_row_index() % 2 == 0 ? 'pt-0' : NULL;
 
@@ -138,25 +134,23 @@ if (have_rows('textboxes')) :
         endif; ?>
 
 
-<section>
-    <div class="wrapper textboxes__repeater <?php echo $pt ?>" id="textboxes__<?php echo get_row_index() ?>">
-        <div class="container-fluid">
-            <div class="row <?php echo get_row_index() % 2 == 0 ? 'd-flex flex-row-reverse' : 'd-flex' ?>">
-                <div class="col-12 d-flex flex-column justify-content-center <?php echo $offset ?>">
-                    <h4><?php echo $pre_title ?></h4>
-                    <h3><?php echo $title ?></h3>
-                    <p class="roboto-light"><?php echo $text ?></p>
-                    <a target="<?php echo esc_attr($button_target); ?>"
-                        href="<?php echo esc_url($button_url); ?>"><?php echo esc_html($button_title); ?></a>
-                </div>
-                <div class="col-12 col-lg-6 pl-0 <?php echo $align ?>">
-                    <img class="img-fluid" loading="lazy" src="<?php echo esc_url($photo['url']); ?>"
-                        alt="<?php echo esc_attr($image['alt']); ?>">
+        <section>
+            <div class="wrapper textboxes__repeater <?php echo $pt ?>" id="textboxes__<?php echo get_row_index() ?>">
+                <div class="container-fluid">
+                    <div class="row <?php echo get_row_index() % 2 == 0 ? 'd-flex flex-row-reverse' : 'd-flex' ?>">
+                        <div class="col-12 d-flex flex-column justify-content-center <?php echo $offset ?>">
+                            <h4><?php echo $pre_title ?></h4>
+                            <h3><?php echo $title ?></h3>
+                            <p class="roboto-light"><?php echo $text ?></p>
+                            <a target="<?php echo esc_attr($button_target); ?>" href="<?php echo esc_url($button_url); ?>"><?php echo esc_html($button_title); ?></a>
+                        </div>
+                        <div class="col-12 col-lg-6 pl-0 <?php echo $align ?>">
+                            <img class="img-fluid" loading="lazy" src="<?php echo esc_url($photo['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-</section>
+        </section>
 
 <?php
     endwhile;
@@ -186,8 +180,7 @@ endif; ?>
                     </p>
                     <?php $ftl_link = get_field('ftl_link'); ?>
                     <?php if ($ftl_link) : ?>
-                    <a href="<?php echo esc_url($ftl_link['url']); ?>"
-                        target="<?php echo esc_attr($ftl_link['target']); ?>"><?php echo esc_html($ftl_link['title']); ?></a>
+                        <a href="<?php echo esc_url($ftl_link['url']); ?>" target="<?php echo esc_attr($ftl_link['target']); ?>"><?php echo esc_html($ftl_link['title']); ?></a>
                     <?php endif; ?>
                 </div>
             </div>
